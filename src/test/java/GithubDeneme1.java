1829b2ff4bda136bc3ca08700c159bf792ea0985
ibrahimkuralozkan <ibrahimkuralozkan@gmail.com>
2021-09-23 08:37:17 +0200

    Git hub deneme 1_3

--- src/test/java/GithubDeneme1.java
+++ src/test/java/GithubDeneme1.java
@@ -2,5 +2,6 @@ public class GithubDeneme1 {
     public static void main(String[] args) {
         System.out.println("Deneme 1");
         System.out.println("Deneme 1_2");
+        System.out.println("Deneme 1_3");
     }
 }
